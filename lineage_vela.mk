1388b11c79d0b302c9623b0bdadef509b94d9531
me-cafebabe <me.cafebabe@gmail.com>
2023-03-07 02:28:00 +0800

    [TEMP] Pause building vela for now, There are few issues with camera
    * The issues are: No Aux camera, Can't focus
    * It'll need some time to bringup stock camera stack
    * The rest of things are really working fine, though
    
    Change-Id: Ia2aa9f1e7567ad1620a093895c580081a090409d

--- lineage_vela.mk
+++ lineage_vela.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+$(error Pause building vela for now, There are few issues with camera)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
