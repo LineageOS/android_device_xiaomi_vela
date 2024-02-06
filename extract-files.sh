434e4f8211656ecc2a2f610e90cd8c42db48becb
Yumi Yukimura <me.cafebabe@gmail.com>
2024-02-06 13:59:49 +0800

    vela: Patch blobs to load libstdc++_vendor.so instead
    Change-Id: Ib6195daf1791dca5d2c5461cb3389c944667ebba

--- extract-files.sh
+++ extract-files.sh
@@ -12,6 +12,11 @@ function blob_fixup() {
             ${PATCHELF} --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
             ;;
     esac
+
+    # For all ELF files
+    if [[ "${1}" =~ ^.*(\.so|\/bin\/.*)$ ]]; then
+        "${PATCHELF_0_17_2}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+    fi
 }
 
 # If we're being sourced by the common script that we called,
