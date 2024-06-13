16172c7baed550135072d53894c6fe43ad32cc32
Michael Bestas <mikeioannina@gmail.com>
2024-06-14 00:26:49 +0300

    vela: Build missing libraries for 14 QPR3
    E vndksupport: Could not load /vendor/lib/hw/camera.qcom.so from default namespace: dlopen failed: library "libutilscallstack.so" not found: needed by /vendor/lib/libcam_meitu.so in namespace (default).
    
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

--- device.mk
+++ device.mk
@@ -27,6 +27,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+PRODUCT_PACKAGES += \
+    libutilscallstack.vendor:32
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
