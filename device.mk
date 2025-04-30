c03c618084b1735e07fc29992a93837f414b9b50
Bruno Martins <bgcngm@gmail.com>
2025-04-30 21:12:33 +0100

    vela: Set camera override format from reserved via soong config
    Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2023 The LineageOS Project
+# Copyright (C) 2022-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -31,6 +31,8 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hidl.memory@1.0.vendor \
     libutilscallstack.vendor:32
+
+$(call soong_config_set,camera,override_format_from_reserved,true)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
